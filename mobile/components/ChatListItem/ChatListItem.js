d6d7620f4061eed740f5ef451e4a369e3805c7ea
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-01-19 12:31:44 -0500

    Add EntryScreen, LoginScreen, and LoadingModal

--- mobile/components/ChatListItem/ChatListItem.js
+++ mobile/components/ChatListItem/ChatListItem.js
@@ -2,23 +2,23 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
 export default function ChatListItem(props) {
-    return (
-        <TouchableOpacity 
-          onPress={() => 
-             props.navigation.navigate('Chat', {matchName: props.matchName})}  
-          style={styles.listItem} className="chatListItem"
-          >
-          <Text>{props.matchName}</Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity 
+      onPress={() => 
+         props.navigation.navigate('Chat', {matchName: props.matchName})}  
+      style={styles.listItem} className="chatListItem"
+      >
+      <Text>{props.matchName}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = {
-    listItem: {
-        textAlign: 'left',
-        padding: '5%',
-        width: '100%',
-        borderStyle: 'solid',
-        borderBottomWidth: '.25%',
-    }
+  listItem: {
+    textAlign: 'left',
+    padding: '5%',
+    width: '100%',
+    borderStyle: 'solid',
+    borderBottomWidth: '.25%',
+  }
 }
