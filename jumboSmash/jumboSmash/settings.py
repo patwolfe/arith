13cd1baf2932b2ec11be7b4f350455c65c00ac1b
Morgan <mrgnmcsmith@gmail.com>
2020-01-21 22:16:03 -0500

    Removed commented out password requirements

--- jumboSmash/jumboSmash/settings.py
+++ jumboSmash/jumboSmash/settings.py
@@ -88,20 +88,6 @@ DATABASES = {
 # Overides default user model
 AUTH_USER_MODEL = "users.CustomUser"
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-# Commenting these out bc they were making life very annoying
-# AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#    },
-#    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-#    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-#    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
-# ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
