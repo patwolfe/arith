80b45f7c84c7e442ea30015106a7c8e1f9fda507
Morgan <mrgnmcsmith@gmail.com>
2020-01-22 21:48:36 -0500

    Changed swipee to swipe_target, smash then skip test

--- jumboSmash/swipe/models.py
+++ jumboSmash/swipe/models.py
@@ -3,14 +3,19 @@ from users.models import CustomUser as User
 
 
 class InteractionManager(models.Manager):
-    def skip(self, swiper, swipee):
-        interaction, _ = self.get_or_create(swiper=swiper, swipee=swipee)
-        interaction.skip_count += 1
-        interaction.save()
+    def skip(self, swiper, swipe_target):
+        """ Records a left (negative) swipe on a profile """
+        interaction, _ = self.get_or_create(swiper=swiper, swipe_target=swipe_target)
+        if interaction.choice != Interaction.SwipeType.SMASH:
+            # You shouldn't be able to skip when you've already smashed,
+            # not sure what the error handling here should be
+            interaction.skip_count += 1
+            interaction.save()
         return interaction
 
-    def smash(self, swiper, swipee):
-        interaction, _ = self.get_or_create(swiper=swiper, swipee=swipee)
+    def smash(self, swiper, swipe_target):
+        """ Records a right (positive) swipe on a profile """
+        interaction, _ = self.get_or_create(swiper=swiper, swipe_target=swipe_target)
         interaction.choice = Interaction.SwipeType.SMASH
         interaction.save()
         return interaction
@@ -22,7 +27,9 @@ class Interaction(models.Model):
         SMASH = "Y"
 
     swiper = models.ForeignKey(User, related_name="swiper", on_delete=models.CASCADE)
-    swipee = models.ForeignKey(User, related_name="swipee", on_delete=models.CASCADE)
+    swipe_target = models.ForeignKey(
+        User, related_name="swipe_target", on_delete=models.CASCADE
+    )
     choice = models.CharField(
         max_length=1, choices=SwipeType.choices, default=SwipeType.SKIP
     )
--- jumboSmash/swipe/tests.py
+++ jumboSmash/swipe/tests.py
@@ -7,36 +7,48 @@ from users.models import CustomUser as User
 class InteractionManagerTests(TestCase):
     fixtures = ["users/dummy_data.json"]
 
-    def test_smash_swipe_new(self):
+    def test_smash(self):
         swiper = User.objects.get(pk=1)
-        swipee = User.objects.get(pk=2)
-        interaction = Interaction.objects.smash(swiper, swipee)
+        swipe_target = User.objects.get(pk=2)
+        interaction = Interaction.objects.smash(swiper, swipe_target)
 
         self.assertEqual(interaction.swiper.id, 1)
-        self.assertEqual(interaction.swipee.id, 2)
+        self.assertEqual(interaction.swipe_target.id, 2)
 
         self.assertEqual(interaction.skip_count, 0)
         self.assertEqual(interaction.choice, Interaction.SwipeType.SMASH)
 
-    def test_skip_swipe_new(self):
+    def test_skip(self):
         swiper = User.objects.get(pk=1)
-        swipee = User.objects.get(pk=2)
-        interaction = Interaction.objects.skip(swiper, swipee)
+        swipe_target = User.objects.get(pk=2)
+        interaction = Interaction.objects.skip(swiper, swipe_target)
 
         self.assertEqual(interaction.swiper.id, 1)
-        self.assertEqual(interaction.swipee.id, 2)
+        self.assertEqual(interaction.swipe_target.id, 2)
 
         self.assertEqual(interaction.skip_count, 1)
         self.assertEqual(interaction.choice, Interaction.SwipeType.SKIP)
 
-    def test_skip_then_smash_swipe(self):
+    def test_skip_then_smash(self):
         swiper = User.objects.get(pk=1)
-        swipee = User.objects.get(pk=2)
-        _ = Interaction.objects.skip(swiper, swipee)
-        interaction = Interaction.objects.smash(swiper, swipee)
+        swipe_target = User.objects.get(pk=2)
+        _ = Interaction.objects.skip(swiper, swipe_target)
+        interaction = Interaction.objects.smash(swiper, swipe_target)
 
         self.assertEqual(interaction.swiper.id, 1)
-        self.assertEqual(interaction.swipee.id, 2)
+        self.assertEqual(interaction.swipe_target.id, 2)
 
         self.assertEqual(interaction.skip_count, 1)
+        self.assertEqual(interaction.choice, Interaction.SwipeType.SMASH)
+
+    def test_smash_then_skip(self):
+        swiper = User.objects.get(pk=1)
+        swipe_target = User.objects.get(pk=2)
+        _ = Interaction.objects.smash(swiper, swipe_target)
+        interaction = Interaction.objects.skip(swiper, swipe_target)
+
+        self.assertEqual(interaction.swiper.id, 1)
+        self.assertEqual(interaction.swipe_target.id, 2)
+
+        self.assertEqual(interaction.skip_count, 0)
         self.assertEqual(interaction.choice, Interaction.SwipeType.SMASH)
