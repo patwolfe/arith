e176d70abe662c7b4647cf8199a60af1f9c8d9e6
Morgan <mrgnmcsmith@gmail.com>
2020-01-25 10:34:55 -0500

    Re-added unmatch route

--- jumboSmash/tests/test_matches.py
+++ jumboSmash/tests/test_matches.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from chat.models import Match
 from rest_framework.test import force_authenticate, APIRequestFactory
 from users.models import CustomUser as User
+from chat.views import Unmatch
 
 
 class ModelManagerTests(TestCase):
@@ -26,18 +27,13 @@ class ModelManagerTests(TestCase):
         """ Unmatches valid match """
 
         match = Match.objects.create_match(1, 2)
-        match = Match.objects.unmatch(match.pk)
+        unmatch = Match.objects.unmatch(match)
 
-        self.assertEqual(match.user_1.id, 1)
-        self.assertEqual(match.user_2.id, 2)
+        self.assertEqual(unmatch.user_1.id, 1)
+        self.assertEqual(unmatch.user_2.id, 2)
 
-        self.assertFalse(match.top5)
-        self.assertTrue(match.unmatched)
-
-    def test_unmatch_invalid(self):
-        """ Attempts to unmatch non-existant match """
-        with self.assertRaises(Match.DoesNotExist):
-            match = Match.objects.unmatch(1)
+        self.assertFalse(unmatch.top5)
+        self.assertTrue(unmatch.unmatched)
 
     def test_list(self):
         """ Confirms list_matches returns all matches for a user """
@@ -52,3 +48,55 @@ class ModelManagerTests(TestCase):
         self.assertEqual(matches[0].id, 1)
 
         self.assertEqual(matches[1].id, 2)
+
+
+class ModelViewsTest(TestCase):
+    fixtures = ["tests/dummy_users.json"]
+
+    def test_unmatch_valid_1(self):
+        """ Confirms user_1 can unmatch match """
+        _ = Match.objects.create_match(1, 2)
+        user = User.objects.get(pk=1)
+
+        factory = APIRequestFactory()
+        request = factory.post("/unmatch/", {"match": 1}, format="json")
+        force_authenticate(request, user=user)
+        view = Unmatch.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+
+        match = Match.objects.get(pk=1)
+        self.assertTrue(match.unmatched)
+
+    def test_unmatch_valid_2(self):
+        """ Confirms user_2 can unmatch match """
+        _ = Match.objects.create_match(1, 2)
+        user = User.objects.get(pk=2)
+
+        factory = APIRequestFactory()
+        request = factory.post("/unmatch/", {"match": 1}, format="json")
+        force_authenticate(request, user=user)
+        view = Unmatch.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+
+        match = Match.objects.get(pk=1)
+        self.assertTrue(match.unmatched)
+
+    def test_unmatch_invalid_user(self):
+        """ User not in match cannot unmatch """
+        _ = Match.objects.create_match(1, 2)
+        user = User.objects.get(pk=3)
+
+        factory = APIRequestFactory()
+        request = factory.post("/unmatch/", {"match": 1}, format="json")
+        force_authenticate(request, user=user)
+        view = Unmatch.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 403)
+
+        match = Match.objects.get(pk=1)
+        self.assertFalse(match.unmatched)
