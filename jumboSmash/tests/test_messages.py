175fb85b60f4278399416d65b76cc3ab56784fe0
mahima-agrawal <mahima.agrawal@tufts.edu>
2020-01-26 15:42:58 -0500

    add list_messages to message model w/ tests

--- jumboSmash/tests/test_messages.py
+++ jumboSmash/tests/test_messages.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
-from chat.models import Message
+from users.models import CustomUser as User
+from chat.models import Message, Match
 
 
 class MessageManagerTest(TestCase):
-    fixtures = ["tests/dummy_users.json", "tests/dummy_matches.json"]
+    fixtures = ["tests/dummy_users.json", "tests/dummy_matches.json", "tests/dummy_messages.json"]
 
-    def test_create_message_valid(self):
-        message = Message.objects.create_message(1, 1, "u up?")
+    def test_create_message(self):
+        match = Match.objects.get(pk=1)
+        user = User.objects.get(pk=1)
+        message = Message.objects.create_message(match, user, "u up?")
 
         self.assertEqual(message.match.id, 1)
         self.assertEqual(message.sender.id, 1)
-
         self.assertEqual(message.content, "u up?")
+
+    def test_list_message(self):
+        match = Match.objects.get(pk=1)
+        list_m = Message.objects.list_messages(match)
+
+        self.assertEqual(len(list_m), 2)
+        self.assertEqual(list_m[0].match.id, 1)
+        self.assertEqual(list_m[1].match.id, 1)
+
