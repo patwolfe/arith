b01c82329fce7e556ef05c5cc18b45cc84705a53
Morgan <mrgnmcsmith@gmail.com>
2020-01-31 10:25:08 -0500

    Users can be created without a password

--- jumboSmash/users/forms.py
+++ jumboSmash/users/forms.py
@@ -4,14 +4,20 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        # Sets password field to optional
+        self.fields["password1"].required = False
+        self.fields["password2"].required = False
+        self.fields["password1"].widget.attrs["autocomplete"] = "off"
+        self.fields["password2"].widget.attrs["autocomplete"] = "off"
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('email',)
+        fields = ("email",)
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ('email',)
+        fields = ("email",)
--- jumboSmash/users/models.py
+++ jumboSmash/users/models.py
@@ -8,20 +8,22 @@ class CustomUserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def create_user(self, email, password):
+    def create_user(self, email):
         user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
+        user.set_unusable_password()
         user.save()
         return user
 
     def create_staffuser(self, email, password):
         user = self.create_user(email, password)
+        user.set_password(password)
         user.is_staff = True
         user.save()
         return user
 
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
+        user.set_password(password)
         user.is_staff = True
         user.is_superuser = True
         user.save()
