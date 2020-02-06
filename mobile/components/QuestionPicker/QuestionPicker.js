59eca04a1c302619192b59587264f87792657020
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-02-06 12:02:36 -0500

    Make questions rendered in QuestionPicker dynamic

--- mobile/components/QuestionPicker/QuestionPicker.js
+++ mobile/components/QuestionPicker/QuestionPicker.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { 
-  StyleSheet,
-  Text, 
-  View } from 'react-native';
+  ScrollView,
+  StyleSheet, 
+} from 'react-native';
 import Question from './Question/Question';
-
+import mockQuestions from 'jumbosmash/test_data/mockQuestions'
 
 export default function QuestionPicker(props) {
   return (
-    <Question question='Favorite Disney Show?'/> 
+    <ScrollView style={styles.questionContainer}>
+      {mockQuestions.questions.map((question, i) => { 
+        return (<Question question={question} key={i}/>);}
+      )}
+    </ScrollView>
   );
 }
 
 const styles =  StyleSheet.create({
-    
+  questionContainer: {
+    marginTop: '10%'
+  }    
 });
---
+++ mobile/test_data/mockQuestions.js
@@ -0,0 +1,8 @@
+export default {
+  questions: [
+    'Favorite Disney Show',
+    'Biggest surprise about college?',
+    'Favorite Tufts Memory',
+    'Dream Dinner Guest'
+  ]
+}
