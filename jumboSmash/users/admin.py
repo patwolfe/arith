6d6087b3a5fffd1b5ac9df306a57e310661fdaad
Steven Song <steven.song98@gmail.com>
2020-02-06 21:14:31 -0500

    base user rework done

--- jumboSmash/users/admin.py
+++ jumboSmash/users/admin.py
@@ -1,31 +1,76 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import User, Profile, Photo
+from django.utils.html import format_html
+from django.urls import re_path, reverse
+from django.http import HttpResponseRedirect
 
 class UserAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "preferred_name", "discoverable", "status", "is_staff")
     ordering = ("email",)
 
-    list_filter = ("status", "is_staff")
+    list_filter = ("status", "status", "is_staff")
     search_fields = ("email", "first_name", "last_name", "preferred_name")
     
-    fieldsets = (
-        ("About", {"fields": ("email", "password", "first_name", "last_name", "preferred_name", "discoverable")}),
-        ("Permissions", {"fields": ("status", "is_staff")})
-    )
+    fieldsets = ()
+    fields = ("email", "first_name", "last_name", "status", "preferred_name", "discoverable")
     add_fieldsets = (
         ("About", {"fields": ("email", "password1", "password2", "first_name", "last_name", "preferred_name", "discoverable")}),
         ("Permissions", {"fields": ("status", "is_staff")})
     )
 
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        user = User.objects.get(id=object_id)
+        extra_context['user_status'] = user.status
+        extra_context['id_photo'] = user.id_photo
+        extra_context['profiles'] = map(lambda profile: profile.get_profile(), Profile.objects.filter(user=object_id))
+        extra_context['photo_sets'] = map(lambda photo_set: photo_set.get_photos(), Photo.objects.filter(user=object_id))
+        return super(UserAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "bio")
+    class Media: 
+        css = {'all': ('users/admin/admin.css',)}
 
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ("user", "path", "approved", "order")
-    ordering = ("user", "order", "-approved")
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            re_path(
+                r'^(?P<user_id>.+)/approve/$',
+                self.admin_site.admin_view(self.approve),
+                name='pending-user-approve',
+            ),
+            re_path(
+                r'^(?P<user_id>.+)/reject/$',
+                self.admin_site.admin_view(self.reject),
+                name='pending-user-reject',
+            ),
+            re_path(
+                r'^(?P<user_id>.+)/ban/$',
+                self.admin_site.admin_view(self.ban),
+                name='pending-user-ban',
+            ),
+            re_path(
+                r'^(?P<user_id>.+)/unban/$',
+                self.admin_site.admin_view(self.unban),
+                name='pending-user-unban',
+            )
+        ]
+        return custom_urls + urls
+
+    def approve(self, request, user_id, *args, **kwargs):
+        User.objects.approve(user_id)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    def reject(self, request, user_id, *args, **kwargs):
+        User.objects.reject(user_id)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    def ban(self, request, user_id, *args, **kwargs):
+        User.objects.ban(user_id)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    def unban(self, request, user_id, *args, **kwargs):
+        User.objects.unban(user_id)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Photo, PhotoAdmin)
--- jumboSmash/users/views.py
+++ jumboSmash/users/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from users.models import User, Profile
-from users.serializers import UserSerializer, ProfileSerializer
+from users.serializers import SimpleUserSerializer, FullUserSerializer
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 class ListUsers(APIView):
@@ -11,17 +11,18 @@ class ListUsers(APIView):
     """
     def get(self, request):
         queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
+        serializer = SimpleUserSerializer(queryset, many=True)
         return Response(serializer.data)
 
 class GetProfile(APIView):
     """
     Get a profile and photos for user
     """
+    # TODO don't show if banned
     def get(self, request):
         user_id = request.query_params.get("id")
-        profile = Profile.objects.get(user=user_id)
-        serializer = ProfileSerializer(profile, context={"request": request})
+        user = User.objects.get(id=user_id)
+        serializer = FullUserSerializer(user, context={"request": request})
         return Response(serializer.data)
 
 class EditProfile(APIView):
@@ -29,4 +30,12 @@ class EditProfile(APIView):
     Edit profile for currently logged in user
     """
     def post(self, request):
-        return Response()
+        user_id = request.user.id
+        serializer = FullUserSerializer(data=request.data, context={"request": request})
+        serializer.is_valid()
+        print("-----------------------------------")
+        print(serializer.validated_data)
+        print("-----------------------------------")
+        user = User.objects.edit(user_id, serializer.validated_data)
+        ret_serializer = FullUserSerializer(user, context={"request": request})
+        return Response(ret_serializer.data)
