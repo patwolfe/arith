0539f6e391a9c87567204de8d7d1dfef5928dc47
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-02-13 00:12:32 -0500

    Working upload of single image to S3

--- mobile/components/CreateProfileWizard/CreateProfileWizard.js
+++ mobile/components/CreateProfileWizard/CreateProfileWizard.js
@@ -12,25 +12,30 @@ import QuestionPicker from 'jumbosmash/components/QuestionPicker/QuestionPicker'
 import urls from 'jumbosmash/constants/Urls';
 
 export default function CreateProfileWizard(props) {
-  const initProfile  = {
-    name: props.userName,
-    pronouns: props.userPronouns,
-    pictures: {},
-    questions: {},
-    stage: 'questions'
+  const initState  = {
+    profile: {
+      name: props.userName,
+      pronouns: props.userPronouns,
+      bio: 'Just testing',
+      pictures: {},
+      questions: {},
+    },
+    stage: 'photos',
+    photoSetInfo: null
   };
-  const [state, dispatch] = useReducer(reducer, initProfile); 
-  let questionPicker = <QuestionPicker dispatch={dispatch}/>;
-  let photoPicker = <PhotoPicker dispatch={dispatch}/>;
+  const [state, dispatch] = useReducer(reducer, initState); 
+  const questionPicker = <QuestionPicker dispatch={dispatch}/>;
+  const photoPicker = <PhotoPicker dispatch={dispatch}/>;
 
   // Fetch s3 urls when component is rendered
   useEffect(() => {
-    getEditProfile();
+    getEditProfile(dispatch);
   }, []);
 
   useEffect(() => {
     if (state.stage === 'done') {
-      //sendProfile();
+      Alert.alert('Submitted!');
+      submitProfile(dispatch, state);
     }
   }, [state]);
 
@@ -38,27 +43,61 @@ export default function CreateProfileWizard(props) {
     <View style={styles.wizard}>
       {state.stage === 'questions' ? questionPicker : photoPicker}
       <Button 
-        title={state.stage === 'questions' ? 'Next' : 'Submit'}
-        onPress={() => 
-        {
-          if (state.stage === 'questions')
-            dispatch({type: 'button', payload: 'photos'});
-          else {
-            Alert.alert('Submitted!');
-            console.log(state);
-          }
-        }
-        }/>
+        title={state.stage === 'questions' ? 'Submit' : 'Next'}
+        onPress={() => dispatch({type: 'button'})}/>
     </View>
   );
 }
 
-async function getEditProfile() {
+async function getEditProfile(dispatch) {
   const url = `${urls.backendURL}user/profile/edit/`;
-  let res = await APICall.GetAuth(url);
-  console.log(res);
+  const result = await APICall.GetAuth(url);
+  // save urls in state
+  dispatch({type: 'updatePhotoSetInfo', photoSetInfo: result.res});
 }
 
+async function submitProfile(dispatch, state) {
+  await uploadPicture(state.pictures[1], state.photoSetInfo.d[1][1].fields);
+  dispatch({type: 'button'});
+  return true;
+  
+}
+
+async function uploadProfile(profile) {
+  return true;
+}
+
+async function uploadPicture(imagePath, photoInfo) {
+  let body = formDataFromPhotoInfo(photoInfo);
+  let filename = imagePath.split('/').pop();
+  const photo = {
+    uri: imagePath,
+    type: 'image/jpg',
+    name: filename
+  };
+  body.append('file', photo);
+  const url = urls.photoBucketURL;
+  await APICall.PostNoAuth(url, {'Content-Type': 'multipart/form-data'}, body);
+  return true;
+}
+
+function formDataFromPhotoInfo(photoInfo) {
+  let body = new FormData();
+
+  // console.log('----AWS Info----');
+  // console.log(`key: ${photoInfo.key}`);
+  // console.log(`AWSAccessKeyId: ${photoInfo.AWSAccessKeyId}`);
+  // console.log(`policy: ${photoInfo.policy}`);
+  // console.log(`signature: ${photoInfo.signature}`);
+  // console.log('---------------');
+
+
+  body.append('key', photoInfo.key);
+  body.append('AWSAccessKeyId', photoInfo.AWSAccessKeyId);
+  body.append('policy', photoInfo.policy);
+  body.append('signature', photoInfo.signature);
+  return body;
+}
 
 function reducer(state, action) {
   switch(action.type) {
@@ -68,15 +107,19 @@ function reducer(state, action) {
     return {...state, questions: {...state.questions, [action.id]: action.payload}};
   case 'button': {
     let nextStep = '';
-    if (state.currStep === 'photos') {
+    if (state.stage === 'photos') {
       nextStep = 'questions';
+    } else if (state.stage === 'done') {
+      nextStep = 'photos';
     } else { 
       nextStep = 'done';
     }
     return {...state, stage: nextStep};
   }
+  case 'updatePhotoSetInfo':
+    return {...state, photoSetInfo: action.photoSetInfo};
   default:
-    throw new Error();
+    throw new Error('Invalid reducer action');
   }
 }
 
