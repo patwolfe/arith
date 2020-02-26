bc0b35a63aaf06c21f75f0c074410d29d743bd73
steven <steven.song98@gmail.com>
2020-02-25 22:47:28 -0500

    collec static script

--- utils/collectstatic.sh
+++ utils/collectstatic.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# get virtual env folder
 cd /var/app/venv
+DIR=""
 
-for D in `find . -mindepth 1 -type d`
+for D in `find . -mindepth 1 -maxdepth 1 -type d`
 do
-    echo D
+    DIR=$D
 done
+
+# activate virtual env
+source $DIR/bin/activate
+
+# run collect static in staging
+cd /var/app/staging/jumboSmash
+python manage.py collecstatic
