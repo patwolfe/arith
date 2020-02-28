409a55afa846aee66c2c36c0176d570c15da1c0d
steven <steven.song98@gmail.com>
2020-02-28 16:22:46 -0500

    sudo user

--- .platform/hooks/postdeploy/run_init.sh
+++ .platform/hooks/postdeploy/run_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./utils/deploy/00_init.sh
-PATH=$(pipenv --venv)
-which mv
-mv $PATH /home/ec2-user/.local/share/virtualenvs/ -p
+#./utils/deploy/00_init.sh
+#PATH=$(pipenv --venv)
+#which mv
+#mv $PATH /home/ec2-user/.local/share/virtualenvs/ -p
+sudo -H -u ec2-user bash -c './utils/deploy/00_init.sh'
