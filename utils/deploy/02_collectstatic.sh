21edb0c79f5e7d386c34ecf8072acf11a3525c9c
steven <steven.song98@gmail.com>
2020-02-28 13:52:41 -0500

    cleanup scripts

--- utils/deploy/02_collectstatic.sh
+++ utils/deploy/02_collectstatic.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
 ./utils/deploy/manage.sh collectstatic
-
-exit 0
-
-
-
-
-
-
-
-
-
-
-# activate virtual env
-pipenv install --skip-lock
-
-# import env variables if not set
-if [[ ! $RDS_HOSTNAME ]] ; then
-  sudo sh -c 'cat /opt/elasticbeanstalk/deployment/env > .env'
-fi
-
-# run collect static
-cd jumboSmash
-pipenv run python manage.py collectstatic
--- utils/deploy/manage.sh
+++ utils/deploy/manage.sh
@@ -5,39 +5,6 @@ if [[ ! $RDS_HOSTNAME ]] ; then
   sudo sh -c 'cat /opt/elasticbeanstalk/deployment/env > .env'
 fi
 
-pipenv run python jumboSmash/manage.py "$@"
-
-exit 0
-
-
-
-
-
-
-
-
-
-
-if [[ $1 = '--usage' ]] ; then
-  echo "Use this script to run manage.py in django app"
-  echo "$ manage.sh runserver"
-  exit 0
+if ! pipenv run python jumboSmash/manage.py "$@" ; then
+  echo "MAKE SURE TO RUN 00_init TO INSTALL VIRTUAL ENV FIRST"
 fi
-
-# ensure command is run in project root for virtual env
-if [[ $PWD = /var/app/current ]] ; then
-  echo "Executing manage command at root of current app"
-else
-  echo "MUST BE IN ROOT OF CURRENT APP (/var/app/current)"
-  exit 1
-fi
-
-# import env variables if not set
-if [[ ! $RDS_HOSTNAME ]] ; then
-  sudo sh -c 'cat /opt/elasticbeanstalk/deployment/env > .env'
-fi
-
-# run commands in virtual env
-cd jumboSmash
-pipenv install --skip-lock
-pipenv run python manage.py $1
