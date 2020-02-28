ffdd0dfb323d0e6569c3276d8453b59efe550ef0
steven <steven.song98@gmail.com>
2020-02-28 12:12:22 -0500

    srcripts

--- utils/collectstatic.sh
+++ utils/collectstatic.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # get virtual env folder
-cd /var/app/venv
-for D in `find . -mindepth 1 -maxdepth 1 -type d`
-do
-    DIR=$D
-done
+# cd /var/app/venv
+# for D in `find . -mindepth 1 -maxdepth 1 -type d`
+# do
+#     DIR=$D
+# done
 
 # activate virtual env
-source $DIR/bin/activate
+# source $DIR/bin/activate
+cd /var/app/staging
+pipenv install --skip-lock
 
 # run collect static in staging
 cd /var/app/staging/jumboSmash
-python manage.py collectstatic
+pipenv run python manage.py collectstatic
 
 # run db migrate for sqlite
-python manage.py makemigrations users swipe chat report
-python manage.py migrate
-
+if [[ $RESETDB = true ]] ; then
+  pipenv run python manage.py makemigrations users swipe chat report
+  pipenv run python manage.py migrate
+fi
