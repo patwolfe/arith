e202f205ccbd051f307f0cfd74473a072858d6e0
steven <steven.song98@gmail.com>
2020-02-28 17:37:17 -0500

    update error msg

--- utils/deploy/manage.sh
+++ utils/deploy/manage.sh
@@ -5,15 +5,14 @@ if [[ ! $RDS_HOSTNAME ]] ; then
   sudo sh -c 'cat /opt/elasticbeanstalk/deployment/env > .env'
 fi
 
-CMD="pipenv run python jumboSmash/manage.py $@"
-echo "RUNNING COMMAND: $CMD"
+pipenv run python jumboSmash/manage.py "$@"
 
 EXIT_CODE=$?
 if [ $EXIT_CODE ] ; then
   echo
   echo "+---------------------------------------------+"
   echo "|                                             |"
-  echo "| MAKE SURE VIRTUAL ENV IS INSTALLED          |"
+  echo "| MAKE SURE THE VIRTUAL ENV IS INSTALLED      |"
   echo "| THIS SCRIPT MUST BE RUN AT THE PROJECT ROOT |"
   echo "|                                             |"
   echo "+---------------------------------------------+"
