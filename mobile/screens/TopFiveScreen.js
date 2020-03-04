ab6b88eacd60ec5db295b5b2d1b11650ba8bdde1
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-03-03 23:58:39 -0500

    added skeleton for getting top 5 names list from backend

--- mobile/screens/TopFiveScreen.js
+++ mobile/screens/TopFiveScreen.js
@@ -1,19 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Autocomplete from 'jumbosmash/components/Autocomplete/Autocomplete';
 import {
-  Button,
   View,
   Text,
   TextInput,
   TouchableOpacity,
   StyleSheet
 } from 'react-native';
+
+import APICall from 'jumbosmash/utils/APICall';
 import Button from 'jumbosmash/components/Button/Button';
+import LoadingModal from 'jumbosmash/components/LoadingModal/LoadingModal';
 import namesList from '../test_data/SeniorNames';
 
 export default function TopFiveScreen() {
   const [names, setNames] = useState(['', '', '', '', '']);
+  const [namesList, setNamesList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [rejected, setRejected] = useState(false);
 
+  useEffect(() => {
+    const getNames = async () => {
+      // let url = `${urls.backendURL}auth/token/`;
+      // let result = await APICall.PostNoAuth(url, 
+      //   {'Content-Type': 'application/x-www-form-urlencoded'},
+      //   `email=${props.email}&token=${state.otp}`
+      // );
+
+      if (result.error || !result.ok) {
+        setLoading(false);
+        setRejected(true);
+        return false;
+      }
+
+      setNamesList(result.res._____);
+      setLoading(false);
+      return true;
+    };
+    
+    // Don't send OTP when component is first rendered
+    getNames();
+  }, []);
   // puts namesList into [{name: 'Patrick'}, {name: 'Caroline'}] format
   // is this going to be super slow and horrible? I don't know
   const namesFormattedList = namesList;
